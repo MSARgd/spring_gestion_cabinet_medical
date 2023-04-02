36faa6ebbdf49fef1244216bf836c134790aa63b
sidiAbdalla <sidabdallard@gmail.com>
2023-04-02 15:30:03 +0000

    Sun Apr  2 03:30:03 PM +00 2023

---
+++ src/main/java/ma/enset/springgestioncabinetmedical/SpringGestionCabinetMedicalApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.springgestioncabinetmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGestionCabinetMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGestionCabinetMedicalApplication.class, args);
+    }
+
+}
