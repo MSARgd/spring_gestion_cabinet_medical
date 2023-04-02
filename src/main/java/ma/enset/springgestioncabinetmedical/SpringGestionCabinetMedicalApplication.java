ac8a6bd73fc792ff74b6e5b83366463363c37fd5
sidiAbdalla <sidabdallard@gmail.com>
2023-04-02 17:06:06 +0000

    Sun Apr  2 05:06:06 PM +00 2023

--- src/main/java/ma/enset/springgestioncabinetmedical/SpringGestionCabinetMedicalApplication.java
+++ src/main/java/ma/enset/springgestioncabinetmedical/SpringGestionCabinetMedicalApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @SpringBootApplication
@@ -28,11 +30,38 @@ public class SpringGestionCabinetMedicalApplication implements CommandLineRunner
                     new Date((long) (Math.random() * System.currentTimeMillis()))
             ));
         }
-
-
-
-
-
+        System.out.println("========================================");
+        List<Patient> patientList = patientRepository.findAll();
+        patientList.forEach(p->{
+            System.out.println(p.toString());
+        });
+        System.out.println("========================================");
+        Patient patient = patientRepository.findById(1L).get();
+        System.out.println(patient.toString());
+        System.out.println("========================================");
+        Patient patient2 = patientRepository.findPatientByName("patient3");
+        System.out.println(patient2.toString());
+        System.out.println("========================================");
+        List<Patient> patientByMaladeTrue = patientRepository.findPatientByMalade(true);
+        System.out.println("Les patient -> malade=true");
+        patientByMaladeTrue.forEach(p->{
+            System.out.println(p.toString());
+        });
+        System.out.println("============================================");
+        System.out.println("Les patient -> malade=false");
+        List<Patient> patientByMaladeFalse = patientRepository.findPatientByMalade(false);
+        patientByMaladeFalse.forEach(p->{
+            System.out.println(p.toString());
+        });
+        System.out.println("========================================");
+        System.out.println("metter a jour un patient");
+        Patient patientToUpdate = patientRepository.findById(2L).get();
+        System.out.println("before update : "+patientToUpdate);
+        patientToUpdate.setScore(100);
+        patientToUpdate.setMalade(false);
+        patientRepository.save(patientToUpdate);
+        System.out.println("after update : "+patientRepository.findById(2L).get().toString());
+        System.out.println("===================================");
 
     }
 }
--- src/main/java/ma/enset/springgestioncabinetmedical/repository/PatientRepository.java
+++ src/main/java/ma/enset/springgestioncabinetmedical/repository/PatientRepository.java
@@ -1,8 +1,13 @@
 package ma.enset.springgestioncabinetmedical.repository;
 
+import jdk.dynalink.linker.LinkerServices;
 import ma.enset.springgestioncabinetmedical.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Patient findPatientByName(String name);
+    List<Patient> findPatientByMalade(Boolean malade);
 
 }
